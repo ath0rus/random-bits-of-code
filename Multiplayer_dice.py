6300aa6204ce8358a00e56f523d7a2620b5955ab
ath0rus <65640442+ath0rus@users.noreply.github.com>
2021-06-02 17:36:45 +1000

    Update Multiplayer_dice.py

--- Multiplayer_dice.py
+++ Multiplayer_dice.py
@@ -2,8 +2,17 @@ import random as rand
 
 n = int(input("Number of Players: ")) #players
 m = int(input("Number of Rounds: ")) #rounds
+player_scores = {}
+
+for i in range(n):
+    player_scores[i+1] = []
 
 for i in range(m):
     print(f"== Round {i+1} ==")
+
     for a in range(n):
-        print(f"P{a+1}: {rand.randint(1, 6)}")
+        score = rand.randint(1, 6)
+        print(f"P{a+1}: {score}")
+        player_scores[(a+1)].append(score)        
+
+print(player_scores)
